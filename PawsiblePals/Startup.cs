892e925c7c913d1c9ab19589985a8af449776b4a
Mark McDonald <markanmcdonald@outlook.com>
2020-01-30 13:21:43 -0600

    Canada

--- PawsiblePals/Startup.cs
+++ PawsiblePals/Startup.cs
@@ -2,7 +2,7 @@
 //Brinatubs
 //Dabs for success.
 // Leave me alone! :)
-//Denice is my favorite.
+//Denice is my favorite. *favourite*
 using System;
 using System.Collections.Generic;
 using System.Linq;
