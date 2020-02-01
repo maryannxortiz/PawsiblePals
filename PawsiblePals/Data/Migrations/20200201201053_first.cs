8feb584e5d4c2ba3f60c7a68e868b3866b3cecee
Mark McDonald <markanmcdonald@outlook.com>
2020-02-01 14:12:44 -0600

    Migration and update-database

---
+++ PawsiblePals/Data/Migrations/20200201201053_first.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PawsiblePals.Data.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    PetID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Age = table.Column<int>(nullable: false),
+                    Weight = table.Column<int>(nullable: false),
+                    Color = table.Column<int>(nullable: false),
+                    Breed = table.Column<int>(nullable: false),
+                    PetFriendly = table.Column<bool>(nullable: false),
+                    PeopleFriendly = table.Column<bool>(nullable: false),
+                    Gender = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.PetID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pets");
+        }
+    }
+}
