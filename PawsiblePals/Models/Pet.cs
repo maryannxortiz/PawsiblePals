2345494c34e6750c241cbd2eb615d1fb00dbc7a5
Maryann <2501maryannxortiz@users.noreply.github.com>
2020-02-03 09:14:01 -0600

    b

--- PawsiblePals/Models/Pet.cs
+++ PawsiblePals/Models/Pet.cs
@@ -8,6 +8,7 @@ namespace PawsiblePals.Models
     //Will create the rows of info, filled by database.
     public class Pet
     {
+        public string PetName { get; set; }
         public int PetID { get; set; }
         public int Age { get; set; }
         public int Weight { get; set; }
