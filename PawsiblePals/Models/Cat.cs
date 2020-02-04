828206bfcdf86e331ed506c4064fed7247a72886
Maryann <2501maryannxortiz@users.noreply.github.com>
2020-02-04 13:10:43 -0600

    testing dropdowns, creating distiction in breedand weight between species. Not done yet.

---
+++ PawsiblePals/Models/Cat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PawsiblePals.Models
+{
+    public class Cat : Pet
+    {
+
+    }
+}
--- PawsiblePals/Models/Pet.cs
+++ PawsiblePals/Models/Pet.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +10,23 @@ namespace PawsiblePals.Models
     //Will create the rows of info, filled by database.
     public class Pet
     {
-        public string PetName { get; set; }
-        public int Species { get; set; }
+        [Key]
         public int PetID { get; set; }
+        public string PetName { get; set; }
+        //public int Species
+        //{
+        //    switch(Species)
+        //    {
+        //    case 'Dog': return
+        //    break;
+        //    case 'Cat': return;
+        //    break;
+        //    }
+
+
+        //}
+
+        public int Species { get; set; }
         public int Age { get; set; }
         public int Weight { get; set; }
         public int Color { get; set; }
@@ -18,5 +34,17 @@ namespace PawsiblePals.Models
         public bool PetFriendly { get; set; }
         public bool PeopleFriendly { get; set; }
         public char Gender { get; set; }
+
+        
     }
+
+
+    //public class dog
+    //{
+    //    readonly int Species = 1;
+    //}
+    //public class cat
+    //{
+    //    readonly int Species = 2;
+    //}
 }
